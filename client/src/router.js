27f5c165df9b9975f326a800c2bf43966eb90043
lashkevichryan <lashkevichrain@gmail.com>
2021-08-30 12:04:40 +0700

    Got rid of url  hash symbol

--- client/src/router.js
+++ client/src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Ping from './components/Ping.vue'
 import HelloWorld from './components/HelloWorld'
 
@@ -16,8 +16,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
+  history: createWebHistory(),
+  routes 
 })
 
 export default router
